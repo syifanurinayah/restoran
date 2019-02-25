d4f9de4dc458a615a508e80dad24c8f730763c5c
syifa nurinayah <syifanurinayah07@gmail.com>
2019-02-25 05:08:13 -0800

    migration

---
+++ database/migrations/2019_02_25_112045_create_order_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('code');
+            $table->integer('user_id')->unsigned();
+            $table->integer('total');
+            $table->date('order_date');
+            $table->integer('payment_method');
+            $table->integer('status');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order');
+    }
+}
